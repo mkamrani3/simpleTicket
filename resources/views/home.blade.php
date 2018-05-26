3f92c9a6aeb47a30596f8bf8ae95c2e486491e4d
hassan <zaaferani.arta@gmail.com>
2018-05-26 16:14:08 +0430

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+    @guest
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                سامانه ی تیکت ساده
+            </div>
+        </div>
+    @else
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -12,4 +19,6 @@
             </div>
         </div>
     </div>
+    @endguest
+
 @endsection
